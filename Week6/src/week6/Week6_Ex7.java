b83c31d49965e3bf933cb403ed9848ff3850c777
Hamza <hamza@localhost.localdomain>
2014-11-12 14:37:19 +0000

    Week6 Commit: Adding finished version of exercise 7

--- Week6/src/week6/Week6_Ex7.java
+++ Week6/src/week6/Week6_Ex7.java
@@ -2,15 +2,20 @@ package week6;
 
 public class Week6_Ex7 
 {
-	public static void main(String[] args) 
+	public static void main(String args[])
 	{
-		System.out.println("Program to print Fibonacci series up to given number\n");
-		
-		int number = Console.askInt("Enter a number: ");
-		
-		for(int i=1; i<=number; i++)
+		System.out.println("Program to print Fibonacci series up to a given number");
+		int num = Console.askInt("Enter a number: "); 
+		int f1; 
+		int f2 = 0; 
+		int f3 = 1;
+	
+		for(int i=1;i<=num;i++)
 		{
-			//System.out.println(fib(i));
+			System.out.print(" "+f3+" ");
+			f1 = f2;
+			f2 = f3;
+			f3 = f1 + f2;
 		}
 	}
 }
