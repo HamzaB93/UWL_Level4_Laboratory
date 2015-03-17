3dd17ad6cf73465320508dba709c9b08753e47bf
Hamza <https://HamzaB93@bitbucket.org/HamzaB93/uwl_compsci4_lab.git>
2015-03-17 18:34:52 +0000

    Java Exercises: ADT, Adding class decleration for Person.java class along with application code in Main.java. Main.java uses the Constructor, Accessors and Transformer methods.

---
+++ AdsActivites/src/adsADT/Main.java
@@ -0,0 +1,46 @@
+package adsADT;
+
+public class Main 
+{
+	/*
+	 * Person class application code
+	 */
+
+	public static void main(String[] args) 
+	{
+		////////// Constructor //////////
+		
+		// Constructing people using Person class
+		Person hamza = new Person("Hamza Bhatti", 21, 1993, "British", 1.77, 11.00);
+		Person john = new Person("John Doe", 28, 1988, "American", 1.80, 9.00);
+		Person homer = new Person("Homer Simpson", 30, 1970, "American", 1.90, 17);
+		Person peter = new Person("Peter Griffin", 30, 1970, "American", 1.90, 20);
+		
+		////////// Accessor //////////
+		
+		// Using the compareTo method
+		if(homer.compareTo (peter) > 0 )
+			System.out.println("Homer is older than Peter\n");
+		else if (homer.compareTo (peter) <  0 )
+			System.out.println("Homer is younger than Peter\n");
+		else 
+			System.out.println("They are the same age\n");
+		
+		// Using toString method to print details out in string format
+		System.out.println("Printing people:");
+		System.out.println(hamza.toString() + "\n" );
+		System.out.println(john.toString() + "\n" );
+		System.out.println(homer.toString() + "\n" );
+		System.out.println(peter.toString() + "\n" );
+		
+		////////// Transformer //////////
+		
+		// Using the changeWeight method
+		// Original details
+		System.out.println(hamza.toString() + "\n");
+		// Changing weight
+		hamza.changeWeight(10);
+		// Details with new weight
+		System.out.println(hamza.toString() + "\n");
+	}
+}
--- AdsActivites/src/adsADT/Person.java
+++ AdsActivites/src/adsADT/Person.java
@@ -3,35 +3,67 @@ package adsADT;
 public class Person 
 {
 	/*
-	 *  This class will be the contract, outlining
-	 *  class declaration
+	 *  Class declaration
 	 */
 	
 	
 	// Data representation should be private
-	private ...; 
+	private int age, yearOfBirth;
+	private String fullName, nationality;
+	private double height;
+	private double weight;
 	
 	////////// Constructor //////////
 	
-	public Person (int age, int yearOfBirth, 
-			String fullName, String nationality, float height,
-			float weight);
-	// construct a person that has an age, year of birth,
-	// full name, nationality, height(meters) and weight(stone)
-	
+	public Person (String fullName, int age, int yearOfBirth, 
+			String nationality, double height,
+			double weight)
+	{	// Construct a person that has an age, year of birth,
+		// full name, nationality, height(meters) and weight(stone)
+		this.fullName = fullName;
+		this.age = age; 
+		this.yearOfBirth = yearOfBirth;
+		this.nationality = nationality;
+		this.height = height;
+		this.weight = weight;
+	}
+
 	////////// Accessor //////////
 	
-	public int compareTo (Person that);
-	// when comparing, return 1 if this person is older than,
-	// that person, return -1 if this person is younger than,
-	// that person, and 0 when they are equal
-	
-	public String toString ();
-	// give the details/ attributes of the person in
-	// String format
+	public int compareTo (Person that)
+	{
+		// When comparing, return 1 if this person is older than,
+		// that person, return -1 if this person is younger than,
+		// that person, and 0 when they are equal
+		
+		//if (this.age > that.age)
+		//	return 1;
+		//if (this.age < that.age)
+		//	return -1;
+		//else
+		//	return 0;
+		return (this.age > that.age ? +1 :
+				this.age < that.age ? -1 :
+				this.age > that.age ? +1 : 0);
+	}
+
+	public String toString ()
+	{
+		// Give the details/ attributes of the person in
+		// String format
+		return("Full Name: " + this.fullName + ", " + "Age: " + this.age 
+				+ ", " +" Year of Birth: " + this.yearOfBirth + ", " 
+				+ "Nationality: " + this.nationality + ", " +
+				"Height(meters): " + this.height +", "+ "Weight(stone): " + this.weight);
+	}
+
 	
 	////////// Transformer //////////
 	
-	public void changeWeight (int weight);
-	// change the weight of the person
+	public void changeWeight (int n)
+	{
+		// change the weight of the person
+		this.weight = n;
+	}
+	
 }
